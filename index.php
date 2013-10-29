86aa27b3112244e05be56959387aeb3373b283c3
LizMatthews <liz@elizabethmatthewsdesign.com>
2013-10-29 13:13:34 -0400

    Created index.php file
    Created and added index.php

---
+++ index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+
+		<!-- Always force latest IE rendering engine (even in intranet) & Chrome Frame
+		Remove this if you use the .htaccess -->
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+
+		<title>LESS STYLES</title>
+		<meta name="description" content="">
+		<meta name="author" content="Liz Matthews">
+
+		<meta name="viewport" content="width=device-width; initial-scale=1.0">
+
+		<!-- Replace favicon.ico & apple-touch-icon.png in the root of your domain and delete these references -->
+		<link rel="shortcut icon" href="/favicon.ico">
+		<link rel="apple-touch-icon" href="/apple-touch-icon.png">
+        
+        <link rel="stylesheet/less" type="text/css" href="css/styles.less">
+        <script src="js/less-1.5.0.min.js"></script>
+        
+	</head>
+
+	<body>
+		<div class="container">
+			<header>
+				<h1>LESS STYLES</h1>
+			</header>
+			<nav>
+				<a href="/">Home</a>
+				<a href="/contact">Contact</a>
+			</nav>
+
+			<section>
+				<h2>Heading 2</h2>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sed luctus dolor. Duis ultricies ullamcorper lectus eu condimentum. 
+					Aliquam malesuada molestie enim, a aliquam metus dictum at. Quisque vel diam lorem. Morbi in aliquam turpis. Suspendisse placerat dolor 
+					justo, sed scelerisque quam suscipit non.<br /><br />
+					
+					Sed id euismod justo. Aenean eu lectus augue. Suspendisse vitae ultricies mauris. Mauris 
+					rutrum, lorem tristique condimentum auctor, lectus eros tempus augue, pellentesque faucibus justo lectus vel urna.</p>
+				<h3>Heading 3</h3>
+				<h4>Heading 4</h4>
+				<h5>Heading 5</h5>
+				<h6>Heading 6</h6>
+				<ul>
+					<li>list item #1</li>
+					<li>list item #2</li>
+					<li>list item #3</li>
+				</ul>
+			</section>
+
+			<footer>
+				<p>&copy; Copyright  by Liz Matthews</p>
+			</footer>
+		</div>
+	</body>
+</html>
